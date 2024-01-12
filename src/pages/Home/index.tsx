cf09cbd04b996c814e6cd4150e72e58e960e9491
Arthur Phillip Silva <artphil.dev@gmail.com>
2024-01-12 18:20:13 -0300

    FEAT: create login logout simple

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,21 @@
+import LoginPage from "./LoginPage";
+import Logout from 'components/Logout/Logout';
+
 function Home() {
+    const user = localStorage.getItem('token');
+
+    function getHomePage() {
+        return (
+            user? 
+            <div>Usuario logado 
+                <Logout></Logout></div>: <LoginPage/> 
+            )
+    }
     return ( 
-        <div>Home</div>
+        <>Home
+            {getHomePage()}
+
+        </>
      );
 }
 
