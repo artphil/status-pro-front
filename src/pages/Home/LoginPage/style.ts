55c231cd07b024f4b3425e0ab7b6aa6a79e84828
Arthur Phillip Silva <artphil.dev@gmail.com>
2024-01-12 18:20:13 -0300

    FEAT: create login logout simple

---
+++ src/pages/Home/LoginPage/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+export const Main = styled.main`
+display: flex;
+height: 100vh;
+    width: 100vw;
+`
+
+export const Content = styled.section`
+background-color: aqua;
+width: 60%;
+`
