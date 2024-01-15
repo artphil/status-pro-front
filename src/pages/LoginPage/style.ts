6bc4540f269eda2184cec7270cc795c02095e6d5
Arthur Phillip Silva <artphil.dev@gmail.com>
2024-01-15 18:50:38 -0300

    style: add border to poligon

--- src/pages/LoginPage/style.ts
+++ src/pages/LoginPage/style.ts
@@ -8,8 +8,9 @@ export const Main = styled.main`
 `
 
 export const ContentLeft = styled.section`
-    background-color: aqua;
+    background-color: var(--color-variant-2);
     width: 60%;
+    border-radius: 0 50px 50px 0;
 `
 
 export const ContentRight = styled.section`
