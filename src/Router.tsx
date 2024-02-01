e26d4c9ea2a6d284bd9d430d0d09eeab7f1d4653
artphil <artphil.dev@gmail.com>
2024-02-01 18:09:53 -0300

    feat: change logout mode

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import HomePage from 'pages/HomePage';
 import LoginPage from 'pages/LoginPage';
+import Logout from 'components/Logout';
 import SignUpPage from 'pages/SignUpPage';
+
+import HomePage from 'pages/HomePage';
 import DeckPage from 'pages/DeckPage';
 
 
@@ -12,6 +14,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/logout" element={<Logout />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/deck" element={<DeckPage />} />
       </Routes>
